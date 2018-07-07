7a17a56911782cf1dae2df5f69f567f18b505c25
jibaole <jibl@knowbox.cn>
2018-07-07 18:57:51 +0800

    +发送方法(单向、同步、异步)

--- src/main/java/com/ieyecloud/springboot/mq/producer/RocketMQTemplate.java
+++ src/main/java/com/ieyecloud/springboot/mq/producer/RocketMQTemplate.java
@@ -2,8 +2,11 @@ package com.ieyecloud.springboot.mq.producer;
 
 import com.aliyun.openservices.ons.api.*;
 import com.aliyun.openservices.ons.api.bean.ProducerBean;
+import com.ieyecloud.springboot.mq.event.MessageEvent;
 import lombok.extern.slf4j.Slf4j;
-
+import org.springframework.util.SerializationUtils;
+import org.springframework.util.StringUtils;
+import javax.annotation.Resource;
 
 /**
  * @author jibaole
@@ -13,27 +16,59 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class RocketMQTemplate {
+
+    @Resource
     private ProducerBean producer;
 
 
-    public void send(String topic, String tag, String body, long delay) {
-        log.info("start to send message. [topic: {}, tag: {}, body: {}, delay: {}]", topic, tag, body, delay);
-        if (topic == null || tag == null || body == null) {
+    public SendResult send(MessageEvent event) {
+        log.info("start to send message. [topic: {}, tag: {}]", event.getTopic(), event.getTag());
+        if (StringUtils.isEmpty(event.getTopic()) || StringUtils.isEmpty(event.getTag()) || null == event.getDomain()) {
             throw new RuntimeException("topic, tag, or body is null.");
         }
-        Message message = new Message(topic, tag, body.getBytes());
+        Message message = new Message(event.getTopic(), event.getTag(), SerializationUtils.serialize(event.getDomain()));
+        SendResult result = this.producer.send(message);
+        log.info("send message success. ", result.toString());
+        return result;
+    }
+
+    public SendResult send(MessageEvent event, long delay) {
+        log.info("start to send message. [topic: {}, tag: {}]", event.getTopic(), event.getTag());
+        if (StringUtils.isEmpty(event.getTopic()) || StringUtils.isEmpty(event.getTag()) || null == event.getDomain()) {
+            throw new RuntimeException("topic, tag, or body is null.");
+        }
+        Message message = new Message(event.getTopic(), event.getTag(), SerializationUtils.serialize(event.getDomain()));
         message.setStartDeliverTime(System.currentTimeMillis() + delay);
         SendResult result = this.producer.send(message);
         log.info("send message success. ", result.toString());
+        return result;
     }
 
-
-    public void sendAsync(String topic, String tag, String body, long delay) {
-        log.info("start to send message async. [topic: {}, tag: {}, body: {}, delay: {}]", topic, tag, body, delay);
-        if (topic == null || tag == null || body == null) {
+    public void sendOneway(MessageEvent event) {
+        log.info("start to send message. [topic: {}, tag: {}]", event.getTopic(), event.getTag());
+        if (StringUtils.isEmpty(event.getTopic()) || StringUtils.isEmpty(event.getTag()) || null == event.getDomain()) {
             throw new RuntimeException("topic, tag, or body is null.");
         }
-        Message message = new Message(topic, tag, body.getBytes());
+        Message message = new Message(event.getTopic(), event.getTag(), SerializationUtils.serialize(event.getDomain()));
+        this.producer.sendOneway(message);
+        log.info("send message success. ");
+    }
+
+    public void sendAsync(MessageEvent event) {
+        log.info("start to send message. [topic: {}, tag: {}]", event.getTopic(), event.getTag());
+        if (StringUtils.isEmpty(event.getTopic()) || StringUtils.isEmpty(event.getTag()) || null == event.getDomain()) {
+            throw new RuntimeException("topic, tag, or body is null.");
+        }
+        Message message = new Message(event.getTopic(), event.getTag(), SerializationUtils.serialize(event.getDomain()));
+        this.producer.sendAsync(message, new DefaultSendCallback());
+    }
+
+    public void sendAsync(MessageEvent event, long delay) {
+        log.info("start to send message. [topic: {}, tag: {}]", event.getTopic(), event.getTag());
+        if (StringUtils.isEmpty(event.getTopic()) || StringUtils.isEmpty(event.getTag()) || null == event.getDomain()) {
+            throw new RuntimeException("topic, tag, or body is null.");
+        }
+        Message message = new Message(event.getTopic(), event.getTag(), SerializationUtils.serialize(event.getDomain()));
         message.setStartDeliverTime(System.currentTimeMillis() + delay);
         this.producer.sendAsync(message, new DefaultSendCallback());
     }
